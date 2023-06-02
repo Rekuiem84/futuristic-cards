9d47ff9ea1b16cd9c8e377493c0bde24712ed8bf
Rekuiem <93848681+Rekuiem84@users.noreply.github.com>
2023-06-02 18:08:56 +0200

    End of the project

--- script.js
+++ script.js
@@ -18,34 +18,4 @@ inputs.forEach((input) => input.addEventListener("input", handleUpdate));
 
 function handleUpdate() {
 	cards.style.setProperty(`--${this.name}`, this.value);
-	localStorage.setItem(`--${this.name}`, this.value);
-}
-
-function getColors() {
-	for (let i = 0; i < inputs.length; i++) {
-		let key = inputs[i].name;
-		let value = inputs[i].value;
-		localStorage.setItem(`--${key}`, value);
-	}
-	console.log(localStorage);
-}
-
-//key = Card1_Color1
-//value = #ff0058
-//value stored at localStorage[`--${key}`]
-
-window.addEventListener("load", getColors);
-window.addEventListener("load", setColors);
-
-function setColors() {
-	inputs.forEach((input) => {
-		for (let i = 0; i < localStorage.length; i++) {
-			let key = localStorage.key(i);
-			let value = localStorage.getItem(key);
-			console.log(value);
-			cards.style.setProperty(`${key}`, value);
-
-			input.value = cards.style.getPropertyValue(`${key}`);
-		}
-	});
 }
